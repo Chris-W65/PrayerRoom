a6ad01ab7d9c41fdca2cd759aa735f4ac8693b18
chrisworsleynz <chris.worsley.nz@gmail.com>
2023-07-07 14:42:03 +1200

    upload website
    Signed-off-by: chrisworsleynz <chris.worsley.nz@gmail.com>

---
+++ app-files/data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20230622_102849_0",
+      "name": "PANO_20230622_102849_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.013456978170793832,
+        "pitch": -0.029120282588667834,
+        "fov": 1.2718286009657667
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.591990831005951,
+          "pitch": 0.004353493930651098,
+          "title": "<a href='https://trello.com/b/YjcydP8M/the-world' target='_blank'><u>The World</u></a>",
+          "text": "Click the link in the title above to enter The World Prayer Station"
+        },
+        {
+          "yaw": -1.8972316214047353,
+          "pitch": -0.023657653594170824,
+          "title": "<a href='https://trello.com/b/nbL8mtjP/pray-for-5' target='_blank'><u>Pray for 5</u></a>",
+          "text": "Click the link in the title above to enter the Pray for 5 Prayer Station"
+        },
+        {
+          "yaw": -1.388208228220254,
+          "pitch": 0.0005588448530104984,
+          "title": "<a href='https://trello.com/b/E4MZGNVC/thanks-praise' target='_blank'><u>Thanks &amp; Praise</u></a>",
+          "text": "Click the link in the title above to enter the Thanks &amp; Praise Prayer Station"
+        },
+        {
+          "yaw": -0.6484738231141876,
+          "pitch": -0.0007310404399092363,
+          "title": "Introduction",
+          "text": "<img src='img/introduction2.png' height='200px' width='280px'>"
+        },
+        {
+          "yaw": -0.3173312459692248,
+          "pitch": -0.12942055958246357,
+          "title": "<a href='https://trello.com/b/QEbuwDeL/jonah-journey' target='_blank'><u>Jonah Journey</u></a>",
+          "text": "Click the link in the title above to enter the Jonah Journey Prayer Station"
+        },
+        {
+          "yaw": 1.414278538159488,
+          "pitch": 0.10988339275278669,
+          "title": "<a href='https://trello.com/b/tEIhMA4b/forgiveness' target='_blank'><u>Forgiveness Prayer Station</u></a>",
+          "text": "Click the link in the title above to enter the Forgiveness Prayer Station"
+        },
+        {
+          "yaw": 2.034218961955996,
+          "pitch": -0.027104755499996358,
+          "title": "<a href='https://trello.com/b/HGOD7f84/our-parish' target='_blank'><u>Our Parish</u></a>",
+          "text": "Click the link in the title above to enter Our Parish Prayer Station"
+        }
+      ]
+    }
+  ],
+  "name": "Prayer Room",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
